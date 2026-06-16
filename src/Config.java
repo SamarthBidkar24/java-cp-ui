7c6f7e767319e0dcfd77818415a55b810e5f6491
SamarthBidkar24 <samarthbidkar2411@gmail.com>
2026-06-16 23:14:17 +0530

    Update ORS API key to a placeholder
    Replaced sensitive ORS API key with a placeholder.

--- src/Config.java
+++ src/Config.java
@@ -3,7 +3,7 @@
  */
 public class Config {
     // GET YOUR FREE KEY AT: https://openrouteservice.org/dev/#/signup
-    public static final String ORS_API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjYzNDliOTI0ZWU2YjRlNzVhNmY3MzQzYWQyMGQ3NmMxIiwiaCI6Im11cm11cjY0In0=";
+    public static final String ORS_API_KEY = "KEY";
     
     // UPI Payment Config
     public static final String MERCHANT_NAME = "SuperMart Retail";
